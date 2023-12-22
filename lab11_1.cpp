1420331d54edf75277b0ffa8d58c58fac3516daa
MorohaZ <141795069+MorohaZ@users.noreply.github.com>
2023-12-22 16:29:45 +0700

    completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,52 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include<string>
+
+using namespace std ;
+
+bool charkEnter(int& counter){
+    if(cin.get()=='\n'){
+        counter++;
+        if(counter==3){
+            return true;
+        }
+    }
+    return false;
+}
+
+char randomGrad(){
+    const char grades[]={'A', 'B', 'C', 'D', 'F','W'};
+    return grades[rand()%sizeof(grades)]; 
+}
+
+char randpl(){
+    const char pl[] = {'+',' '};
+    return pl[rand()%sizeof(pl)];
+}
+
+int main(){
+    cout<<"Press Enter 3 times to reveal your future.\n";
+     int entercount = 0;
+     char grade;
+
+     srand(time(0));
+
+     while (!charkEnter(entercount))
+     {
+        
+     }
+     char graderand = randomGrad();
+     string pls ; 
+     if (graderand=='B'||graderand=='C'||graderand=='D'){
+        pls = randpl();
+        if(pls == "+"){
+            pls =pls+" ";
+        }
+     }
+     else{
+        pls = " ";
+     }
+    
+     cout << "You will get " << graderand << pls << "in this 261102.";
+}
